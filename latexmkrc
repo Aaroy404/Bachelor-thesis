65b374a79b3f7a0f8902874826bdbb54db378708
Aaroy404 <114906114+Aaroy404@users.noreply.github.com>
2024-03-24 12:21:38 +0100

    vlna

--- latexmkrc
+++ latexmkrc
@@ -1,2 +1,3 @@
 $makeindex = "texindy %O -I latex -C utf8 -L english -o %D %S";
-$pdflatex = "pdflatex --shell-escape %O %S"
+$pdflatex = "pdflatex --shell-escape %O %S";
+$pdflatex = $pdflatex = "bash run_vlna.sh %S";
--- run_vlna.sh
+++ run_vlna.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-vlna -r -l -v KkSsVvZzOoUuAaIi fi-pdflatex.tex
+# Check if a filename is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <filename.tex>"
+    exit 1
+fi
+
+# Store the filename from the command-line argument
+filename="$1"
+
+# Apply vlna to the LaTeX file
+vlna -r -l -v KkSsVvZzOoUuAaIi $filename
+
+# Compile the LaTeX document
+pdflatex --shell-escape -interaction=nonstopmode "$filename"
+
+mv "${filename%.*}.pdf" "output.pdf"
